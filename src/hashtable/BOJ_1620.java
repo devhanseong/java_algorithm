4fd2e904654e19e6f502be4de35ed61b471f38e7
HanseongBae <bha4388@naver.com>
2024-05-01 14:52:31 +0900

    큐, 덱, 해시테이블 기본 문제

---
+++ src/hashtable/BOJ_1620.java
@@ -0,0 +1,43 @@
+package hashtable;
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ_1620 {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int length = Integer.valueOf(st.nextToken());
+        int times = Integer.valueOf(st.nextToken());
+
+        Map<String, String> mapOfString = new HashMap<>();
+        Map<String, String> mapOfNumber = new HashMap<>();
+
+        generateDictionaries(length, mapOfString, mapOfNumber);
+        output(times, mapOfString, mapOfNumber);
+    }
+
+    private static void generateDictionaries(int length, Map<String, String> mapOfString, Map<String, String> mapOfNumber) throws Exception {
+        for (int i = 0; i < length; i++) {
+            String nameOfPocketMon = br.readLine();
+            mapOfString.put(nameOfPocketMon, String.valueOf(i + 1));
+            mapOfNumber.put(String.valueOf(i + 1), nameOfPocketMon);
+        }
+    }
+
+    private static void output(int times, Map<String, String> mapOfString, Map<String, String> mapOfNumber) throws Exception {
+        for (int i = 0; i < times; i++) {
+            String input = br.readLine();
+
+            if (mapOfString.containsKey(input)) {
+                bw.write(mapOfString.get(input) + "\n");
+            } else {
+                bw.write(mapOfNumber.get(input) + "\n");
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+}
---
+++ src/queue/BOJ_18258.java
@@ -0,0 +1,55 @@
+package queue;
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ_18258 {
+    private static final Deque<String> q = new LinkedList<>();
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    private static final String PUSH = "push";
+    private static final String POP = "pop";
+    private static final String SIZE = "size";
+    private static final String EMPTY = "empty";
+    private static final String FRONT = "front";
+    private static final String BACK = "back";
+
+    public static void main(String[] args) throws Exception {
+        int times = Integer.valueOf(br.readLine());
+        for (int i = 0; i < times; i++) {
+            doSomething(br.readLine());
+        }
+        output();
+    }
+
+    private static void doSomething(String input) throws Exception {
+        StringTokenizer st = new StringTokenizer(input);
+        String command = st.nextToken();
+
+        if (command.equals(PUSH)) {
+            q.add(st.nextToken());
+        } else if (!q.isEmpty() && command.equals(FRONT)) {
+            bw.write(q.peek() + "\n");
+        } else if (q.isEmpty() && command.equals(FRONT)) {
+            bw.write("-1\n");
+        } else if (command.equals(SIZE)) {
+            bw.write(q.size() + "\n");
+        } else if (command.equals(EMPTY)) {
+            bw.write(q.isEmpty() ? "1\n" : "0\n");
+        } else if (!q.isEmpty() && command.equals(POP)) {
+            bw.write(q.remove() + "\n");
+        } else if (q.isEmpty() && command.equals(POP)) {
+            bw.write("-1\n");
+        } else if (!q.isEmpty() && command.equals(BACK)) {
+            bw.write(q.peekLast() + "\n");
+        } else if (q.isEmpty() && command.equals(BACK)) {
+            bw.write("-1\n");
+        }
+    }
+
+    private static void output() throws Exception {
+        bw.flush();
+        bw.close();
+    }
+}
