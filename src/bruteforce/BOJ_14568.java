04b22a78f61d6fd66c7701bf27a914f104c29694
HanseongBae <bha4388@naver.com>
2024-05-20 22:26:02 +0900

    백준 - 완전탐색 문제

---
+++ src/bruteforce/BOJ_14568.java
@@ -0,0 +1,29 @@
+package bruteforce;
+
+import java.io.*;
+
+public class BOJ_14568 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int candy = Integer.valueOf(br.readLine());
+        int count = 0;
+
+        for (int a = 1; a <= candy; a++) {
+            for (int b = 1; b <= candy; b++) {
+                for (int c = 1; c <= candy; c++) {
+                    if (a >= b + 2 && c % 2 == 0) {
+                        if (a + b + c == candy) {
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+        bw.write(count + "\n");
+        bw.flush();
+        bw.close();
+    }
+}
---
+++ src/bruteforce/BOJ_19532.java
@@ -0,0 +1,29 @@
+package bruteforce;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_19532 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] nums = new int[6];
+        for (int i = 0; i < 6; i++) {
+            nums[i] = Integer.valueOf(st.nextToken());
+        }
+
+        for (int x = -999; x < 1000; x++) {
+            for (int y = -999; y < 1000; y++) {
+                if (nums[0] * x + nums[1] * y == nums[2]) {
+                    if (nums[3] * x + nums[4] * y == nums[5]) {
+                        bw.write(x + " " + y + "\n");
+                    }
+                }
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+}
